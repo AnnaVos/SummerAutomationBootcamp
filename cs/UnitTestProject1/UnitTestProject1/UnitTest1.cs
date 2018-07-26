c79831819e43a25c2b79e26b172011a7a35d6a97
Anna Vos <anna.vos@polteq.com>
2018-07-26 11:10:21 +0200

    First c sharp exercise

---
+++ cs/UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TechblogLoginTest
+    { 
+    
+        /* private IWebDriver driver;
+        private string uri = "https:techblog.polteq.com/testshop";
+
+         [TestInitialize]
+           public void TestSetup()
+        {
+            
+            driver.Navigate().GoToUrl(uri);
+            driver.Manage().Window.Maximize();
+            // driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 0, 10));
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+           driver.Quit();
+        } */
+
+        [TestMethod]
+        public void LoginAsUser()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https:techblog.polteq.com/testshop");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.ClassName("login")).Click();
+            driver.FindElement(By.Id("email")).SendKeys("anna.tester@polteq.com");
+            driver.FindElement(By.Id("passwd")).SendKeys("Welkom123!");
+            driver.FindElement(By.Id("SubmitLogin")).Click();
+        }
+        [TestMethod]
+        public void IsLoggedIn()
+        {
+            bool isLoggedIn = false;
+            Assert.IsTrue(isLoggedIn, "After entering username and password I should be logged in ");
+
+            //String infoAccount = driver.FindElement(By.ClassName("info-account")).GetText();
+            // Assertions.assertThat(infoAccount).as ("Welcome message contains").contains("Welcome to your account.");
+
+        }
+
+        [TestMethod]
+        public void CheckWelcomeStringStartsWith()
+        {
+            string welcomeText = "Welcome to your account.";
+            StringAssert.StartsWith(welcomeText, "Test", "First comes Welcome");
+        }
+
+    }
+
+}
+ 
